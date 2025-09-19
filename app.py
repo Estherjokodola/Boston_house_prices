e045daca3a55913553275e5cfe6c9e516291bc20
Esther Jokodola <estherjokodola21@gmail.com>
2025-09-19 23:42:12 +0100

    Fix: import escape from markupsafe instead of flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template, redirect,flash, session, escape
+from markupsafe import escape
 
 import numpy as np
 import pandas as pd
